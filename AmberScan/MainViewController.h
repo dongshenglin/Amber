d97189714d2a0e1dce742a11a0f71317472cf496
dsl <dsl@localhost>
2016-09-19 15:24:04 +0800

    main

---
+++ AmberScan/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  AmberScan
+//
+//  Created by dsl on 16/9/19.
+//  Copyright © 2016年 dsl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ AmberScan/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  AmberScan
+//
+//  Created by dsl on 16/9/19.
+//  Copyright © 2016年 dsl. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCa7Q77sYUsfaY9SgnuqqGlXKeC3JjsNn5VIVYZ8HQz/63Ubbb5wdFOkKQFLANLD2OOWq4VYNRb0jX7x1h/yp+ojpMOlGjnFQtgUYRg7+e4emww63/47KzbDskKwosa9MazmMj0i+rkBTC18N02yQx0souQGvVS3giiuKrJZQl1ajhXGJE1B10SfK1NzYdur6f710EvdyM0CFa61v3JVFGnDipn2IBnJrvwAs7ojO4gMoBMRFcEfJx6HEoUDZXCxw6kTpNjzpudBSxe8GTmsCgX12158xPidL6mYmIODy2YRxtddUMKPlf4i7U7CnnzqKF8kcXnbua1HrGV6J4iBqlb dsl19872145@163.com
+@end
